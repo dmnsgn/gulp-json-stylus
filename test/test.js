cd956a5c1ce381abd5627bd984f0f537bcb75904
Damien SEGUIN <dmnsgn@gmail.com>
2015-07-21 22:58:44 +0100

    Update tests

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var gulpJsonStylus = require('../');
 
-it('should process the data', function (cb) {
+it('should process the data', function(cb) {
   var stream = gulpJsonStylus({
     namespace: 'test'
   });
 
-  stream.on('data', function (file) {
+  stream.on('data', function(file) {
     assert.equal(file.contents.toString(), fs.readFileSync(__dirname + '/variables.styl').toString());
   });
 
